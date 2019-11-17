77ec4ee6bfb650f553b84f6efda34dd6d2da2319
BenjaminDUTOT <benjamin.dutot@esigetel.net>
2019-11-17 15:27:52 +0100

    Initial commit

---
+++ Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Evolutionary
+{
+    public class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Point(int X, int Y)
+        {
+            this.x = X;
+            this.y = Y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AI_Evolutionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
